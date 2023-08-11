5f097ecb6a64ad0b138dfeac9982be61a34415cf
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2023-08-11 20:47:25 +0000

    Update SettingsToggle.tsx

--- src/components/modals/SettingsToggle.tsx
+++ src/components/modals/SettingsToggle.tsx
@@ -14,33 +14,39 @@ export const SettingsToggle = ({
   description,
 }: Props) => {
   const toggleHolder = classnames(
-    'w-14 h-8 flex shrink-0 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out cursor-pointer',
+    'w-14 h-8 flex shrink-0 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out',
     {
       'bg-green-400': flag,
     }
   )
   const toggleButton = classnames(
-    'bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out cursor-pointer',
+    'bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out',
     {
       'translate-x-6': flag,
     }
   )
 
+  const camelCase = (str: string): string =>
+    str.replace(/ (.)/g, (match) => match.toUpperCase()).replace(/ /g, '')
+
   return (
     <>
-      <div className="flex justify-between gap-4 py-3">
-        <div className="mt-2 text-left text-gray-500 dark:text-gray-300">
-          <p className="leading-none">{settingName}</p>
-          {description && (
-            <p className="mt-1 text-xs text-gray-500 dark:text-gray-300">
-              {description}
-            </p>
-          )}
+      <label className="switch-card items-center py-3 text-left dark:text-gray-100">
+        <div>
+          {settingName}
+          <p className="mt-1 text-xs text-gray-500 dark:text-gray-300">
+            {description}
+          </p>
         </div>
-        <div className={toggleHolder} onClick={() => handleFlag(!flag)}>
-          <div className={toggleButton} />
+        <div className={toggleHolder}>
+          <input
+            data-id={camelCase(settingName)}
+            onClick={() => handleFlag(!flag)}
+            type="checkbox"
+            className={toggleButton}
+          />
         </div>
-      </div>
+      </label>
     </>
   )
 }
